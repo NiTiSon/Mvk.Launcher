bd8648c729348c0f881791b99a88c41ac03fde0e
NiTiSon <NiTiSon@users.noreply.github.com>
2023-10-22 16:59:17 +0300

    Scale up header buttons

--- src/Mvk.Launcher/App.xaml.cs
+++ src/Mvk.Launcher/App.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using System.Windows;
 
 namespace Mvk.Launcher;
+
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application
 {
-	internal static double HeaderHeight = 35;
 }
---
+++ src/Mvk.Launcher/Controls/GameInstanceCard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Mvk.Launcher.Controls;
+
+public partial class GameInstanceCard : UserControl
+{
+	public static readonly DependencyProperty ImageProperty;
+
+	public GameInstanceCard()
+	{
+		InitializeComponent();
+	}
+
+	public ImageSource Image
+	{
+		get { return (ImageSource)GetValue(ImageProperty); }
+		set { SetValue(ImageProperty, value); }
+	}
+
+	static GameInstanceCard()
+	{
+		ImageProperty = DependencyProperty.Register(nameof(Image), typeof(ImageSource), typeof(GameInstanceCard));
+	}
+}
